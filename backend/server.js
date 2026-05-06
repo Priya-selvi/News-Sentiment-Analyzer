1a655f1f1de3e37d151fb8def3867d8f1a6128aa
Priya-selvi <2315041@nec.edu.in>
2026-05-06 17:33:52 +0530

    Updated frontend API URL

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 initPassport(); // initialize after dotenv
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://news-sentiment-analyzer-2.onrender.com/api/news', credentials: true }));
 app.use(express.json());
 app.use(passport.initialize());
 
